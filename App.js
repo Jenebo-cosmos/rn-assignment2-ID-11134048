f1c7c8f44dd5c2e4693ede37a7c2fab4d8af3be3
Jenebo-cosmos <167387956+Jenebo-cosmos@users.noreply.github.com>
2024-05-26 00:41:01 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.bold}>YourName</Text>
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+     
+  },
+  text: {
+    fontSize: 24,
+
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
